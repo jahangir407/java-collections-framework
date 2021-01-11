3da4f9aa81ad65fe2616694cef1efb2a387e3ba5
Jahangir Hossain <jahangir.hossain.92@icloud.com>
2021-01-11 18:52:23 +0900

    java-collections framework test project is initiated

---
+++ src/com/jhub/org/Main.java
@@ -0,0 +1,14 @@
+package com.jhub.org;
+
+/**
+ * Java collections framework test project is initiated.
+ * @author Md Jahangir Hossain Bhuyain
+ * @version 0.1
+ * @since Jan 11, 2021
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to java collections framework world...!");
+    }
+}
